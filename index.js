9648d3a06adc9b8b09ce9c1f24825a907bacfb40
Andrea Parodi <andrea@parro.it>
2017-11-28 21:24:34 +0100

    first version

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-export default async function* aiMerge(data) {
-  for (const item of data) {
-    yield item;
+import AsyncIterable from "asynciterable";
+
+const iterateTo = write => async iterable => {
+  const generator = iterable[Symbol.asyncIterator] || iterable[Symbol.iterator];
+  const iterator = generator.call(iterable);
+  let item = await iterator.next();
+  while (!item.done) {
+    write(await item.value);
+    item = await iterator.next();
   }
+};
+
+export default function aiMerge(...sources) {
+  return new AsyncIterable(async (write, end) => {
+    const allSourcesDone = [...sources].map(iterateTo(write));
+    await Promise.all(allSourcesDone);
+    end();
+  });
 }
--- test.js
+++ test.js
@@ -1,5 +1,24 @@
 import test from "tape-async";
 import aiMerge from ".";
+import delay from "delay";
+import asFullfills from "ai-asfullfills";
+import concat from "ai-concat";
+
+function buildIterable(arr) {
+  const iterable = arr.map(v => delay(v * 10).then(() => v));
+  return asFullfills(iterable);
+}
+
+test("merge iterables in parallel", async t => {
+  const iterable = aiMerge(
+    buildIterable([3, 2, 1]),
+    buildIterable([6, 4, 5]),
+    buildIterable([10, 9, 8, 7])
+  );
+
+  const result = await concat.obj(iterable);
+  t.deepEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], result);
+});
 
 test("exports a function", async t => {
   t.is(typeof aiMerge, "function");
